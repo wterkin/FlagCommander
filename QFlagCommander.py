99d0cb130df46df5b56e1b0943754bfaf042ee42
wterkin <appmailbox@mail.ru>
2020-01-06 16:22:51 +0300

    Добавлена метка в статусбар

--- QFlagCommander.py
+++ QFlagCommander.py
@@ -3,16 +3,15 @@
 
 import sys
 
-try:
-    from PySide import QtCore
-    from PySide import QtWidgets as qt
-except:
-    from PyQt5.QtCore import pyqtSlot as Slot
+#try:
+#    from PySide import QtCore
+#    from PySide import QtWidgets as qt
+#except:
 #    from PyQt5 import QtCore
-    from PyQt5 import QtWidgets as qt
-
-from PyQt5 import QtCore as qtcore 
-from PyQt5 import QtWidgets as qtwid
+#from PyQt5 import QtWidgets as qt
+#from PyQt5.QtCore import pyqtSlot as Slot
+from PyQt5 import QtCore as core
+from PyQt5 import QtWidgets as widgets
 
 #from PyQt5 import QtGui as qtgui
 import mainwindow as fc_mainwin
@@ -22,24 +21,29 @@ import sqlite3
 TIMER_PERIOD = 1000 # ms
 POLL_FOLDER_EACH = 10 # 60 # in periods
 
-class QFCMainWindow(qt.QMainWindow, fc_mainwin.Ui_MainWindow):
+class QFCMainWindow(widgets.QMainWindow, fc_mainwin.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.mo_connection = sqlite3.connect("afc2.db")
         # *** Таймер
-        self.mo_timer = qtcore.QTimer()
+        self.mo_timer = core.QTimer()
         self.mo_timer.timeout.connect(self.__on_timer)
         self.mo_timer.start(TIMER_PERIOD)
         self.mo_timer_count = 0
         # *** Прогрессбар
-        self.mo_progress = qt.QProgressBar()
+        self.mo_progress = widgets.QProgressBar()
         self.mo_progress.format = "%v"
-        self.mo_progress.setOrientation(qtcore.Qt.Horizontal)
-        self.mo_progress.setAlignment(qtcore.Qt.AlignLeft)
+        self.mo_progress.setOrientation(core.Qt.Horizontal)
+        self.mo_progress.setAlignment(core.Qt.AlignLeft)
         self.mo_progress.setRange(0, POLL_FOLDER_EACH)
         self.mo_progress.setValue(0)
         self.statusbar.addPermanentWidget(self.mo_progress,0)
+        # *** Метка для вывода состояния
+        self.mo_status = widgets.QLabel()
+        self.mo_status.setAlignment(core.Qt.AlignLeft)
+        self.mo_status.setText("Idle.")
+        self.statusbar.addPermanentWidget(self.mo_status,0)
 
         # *** Сигналы
         self.act_add_flag.triggered.connect(
@@ -70,11 +74,12 @@ class QFCMainWindow(qt.QMainWindow, fc_mainwin.Ui_MainWindow):
 
     def poll_folder(self):
         """Процедура проверяет наличие флагов в папке"""
+        self.mo_status.setText("Checking...")
         pass
 
 def main():
     """Запускающая процедура."""
-    lo_app = qt.QApplication(sys.argv)
+    lo_app = widgets.QApplication(sys.argv)
     lo_mainwin = QFCMainWindow()
     lo_mainwin.show()
     lo_app.exec()
