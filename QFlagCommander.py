b01a00941c437df9c26e473d138e7d5eb514785d
wterkin <appmailbox@mail.ru>
2020-01-07 18:21:53 +0300

    Добавлен запрос для выборки флага из базы

--- QFlagCommander.py
+++ QFlagCommander.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 
 #try:
 #    from PySide import QtCore
-#    from PySide import QtWidgets as qt
+# from PySide import QtWidgets as qt
 #except:
 #    from PyQt5 import QtCore
 #from PyQt5 import QtWidgets as qt
@@ -20,6 +21,8 @@ import sqlite3
 #MAINFORM_UI = "mainform.ui"
 TIMER_PERIOD = 1000 # ms
 POLL_FOLDER_EACH = 10 # 60 # in periods
+FLAG_FOLDER = "/media/data/Projects/python/afc2/flags"
+FLAG_EXT = ".flg"
 
 class QFCMainWindow(widgets.QMainWindow, fc_mainwin.Ui_MainWindow):
     def __init__(self):
@@ -43,7 +46,7 @@ class QFCMainWindow(widgets.QMainWindow, fc_mainwin.Ui_MainWindow):
         self.mo_status = widgets.QLabel()
         self.mo_status.setAlignment(core.Qt.AlignLeft)
         self.mo_status.setText("Idle.")
-        self.statusbar.addPermanentWidget(self.mo_status,0)
+        self.statusbar.addPermanentWidget(self.mo_status,1)
 
         # *** Сигналы
         self.act_add_flag.triggered.connect(
@@ -62,6 +65,7 @@ class QFCMainWindow(widgets.QMainWindow, fc_mainwin.Ui_MainWindow):
     def __delete_flag_action_triggered(self):
         pass
 
+
     def __on_timer(self):
         """Обработчик таймера."""
         self.mo_timer_count +=1
@@ -72,10 +76,45 @@ class QFCMainWindow(widgets.QMainWindow, fc_mainwin.Ui_MainWindow):
             self.mo_progress.reset()
             self.poll_folder()
 
+
     def poll_folder(self):
         """Процедура проверяет наличие флагов в папке"""
         self.mo_status.setText("Checking...")
-        pass
+        # *** Читаем папку с флагами
+        lo_files = os.listdir(FLAG_FOLDER)
+        lo_flags = filter(lambda x: x.endswith(FLAG_EXT), lo_files)
+        for ls_flag in lo_flags:
+            pass
+
+    def query_flag(self, ps_flagname):
+        """Возвращает данные флага."""
+        assert ps_flagname is not None, """Assert:
+            [QFCMainWindow.query_flag]: No <ps_flagname> parameter
+            specified!"""
+
+        ls_sql = """select   F.ftype as aflagtype
+        , F.fcondition as aflagcondition
+            , F.fevent as aflagevent
+            , F.fname as aflagname
+            , F.fkill_after_use as aflagkillafteruse
+            , T.fname as atypename
+            , T.fcode as atypecode
+            , C.fname as aconditionname
+            , C.fcode as aconditioncode
+            , E.fname as aeventname
+            , E.fvalue as aeventvalue
+   from tbl_flags F
+   inner join tbl_types T
+       on     (T.id = F.ftype)
+              and (T.fstatus>0)
+   inner join tbl_conditions C
+       on     (C.id = F.fcondition)
+              and (C.fstatus>0)
+   inner join tbl_events E
+       on     (E.id = F.fevent)
+              and (E.fstatus>0)
+   where     (F.fname = %(pflagname)s)
+         and (F.fstatus>0)"""
 
 def main():
     """Запускающая процедура."""
